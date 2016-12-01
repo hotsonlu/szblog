19c0f8838df08054b5a9d0d7b18648de7f5d1daa
Hotson lu <learning2010@gmail.com>
2016-12-01 17:31:21 +0800

    put markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+
+require 'redcarpet'
+require 'rouge'
+require 'rouge/plugins/redcarpet'
+
+class HTML < Redcarpet::Render::HTML
+  include Rouge::Plugins::Redcarpet
+end
+
+  def markdown(text)
+    render_options = {
+      filter_html:     true,
+      hard_wrap:       true,
+      link_attributes: { rel: 'nofollow' }
+    }
+    renderer = HTML.new(render_options)
+
+    extensions = {
+      autolink:           true,
+      fenced_code_blocks: true,
+      lax_spacing:        true,
+      no_intra_emphasis:  true,
+      strikethrough:      true,
+      superscript:        true
+    }
+    Redcarpet::Markdown.new(renderer, extensions).render(text).html_safe
+  end
 end
