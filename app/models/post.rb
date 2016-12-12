34847eb879280be76bf650c9cf8293ef9ab09409
Hotson lu <learning2010@gmail.com>
2016-12-12 17:01:15 +0800

    add latest posts on index page

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,10 @@ class Post < ApplicationRecord
 
    has_many :posts_tags, class_name: 'PostsTags'
    has_many :tags, through: :posts_tags
+
+  #  def visited
+  #   self.visited_count += 1
+  #   self.save
+  #   self.visited_count
+  # end
 end
