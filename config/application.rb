fd4361a9bde78b43a4164992ebb3114f764e6de1
Hotson lu <learning2010@gmail.com>
2016-12-14 18:28:21 +0800

    fix lib autoload

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,6 @@ module Szblog
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.autoload_paths << "#{Rails.root}/lib"
+    config.eager_load_paths += %W(#{config.root}/lib/)
   end
 end
