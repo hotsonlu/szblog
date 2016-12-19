a9a8cc47ccacb1109023bc26abf135ea98b90410
Hotson lu <learning2010@gmail.com>
2016-12-19 11:32:41 +0800

    sub domainname

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :domain, 'shenzhenlist.com'
+set :domain, 'blog.shenzhenlist.com'
 set :deploy_to, '/data/www/szblog'
 set :repository,  'git@github.com:hotsonlu/szblog.git'
 set :branch, 'master'
